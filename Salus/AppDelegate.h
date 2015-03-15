af3795333eb5b7a7786a985f02dabde898ea08af
stampsrule <stampsrule@gmail.com>
2015-03-15 02:02:29 -0600

    Initial Commit

---
+++ Salus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Salus
+//
+//  Created by Daniel Bell on 2015-03-15.
+//  Copyright (c) 2015 BellTechMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Salus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Salus
+//
+//  Created by Daniel Bell on 2015-03-15.
+//  Copyright (c) 2015 BellTechMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
